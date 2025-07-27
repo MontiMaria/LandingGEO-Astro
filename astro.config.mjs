ea03716c3fb242fd9b99efeca831732126246412
Mar <mar.u1998@hotmail.com>
2025-07-27 13:51:47 -0300

    Reinstalando tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // astro.config.mjs
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  // 1. Aquí le decimos a Vite que use el plugin de Tailwind v4
   vite: {
     plugins: [tailwindcss()],
   },
-  
-  // 2. Mantenemos la configuración para GitHub Pages
   output: 'static',
   site: 'https://MontiMaria.github.io',
   base: '/LandingAstro',
